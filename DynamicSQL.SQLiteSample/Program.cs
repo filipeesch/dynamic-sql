7da989faaac4b787182a50e024577860c8f5bb49
Filipe Esch <filipe.esch@farfetch.com>
2024-01-18 11:36:17 +0000

    create output result rendering

---
+++ DynamicSQL.SQLiteSample/Program.cs
@@ -0,0 +1,26 @@
+﻿using DynamicSQL.Compiler;
+using Microsoft.Data.Sqlite;
+
+var statement = StatementCompiler.Compile<QueryInput>(
+    i =>
+        $"""
+         SELECT
+            {i.Id} AS Id,
+            {i.Name} AS Name,
+            {i.Age} AS Age
+         """);
+
+var input = new QueryInput(Guid.NewGuid(), "Name_Value", 39);
+
+await using var connection = new SqliteConnection("Data Source=:memory:");
+
+await foreach (var item in statement.QueryAsyncEnumerable<QueryResult>(connection, input))
+{
+    Console.WriteLine($"{item.Id}\t\t{item.Name}\t\t{item.Age}");
+}
+
+Console.ReadLine();
+
+public record QueryInput(Guid Id, string Name, int Age);
+
+public record QueryResult(Guid Id, string Name, int Age);
---
+++ DynamicSQL/ReaderHelper.cs
@@ -0,0 +1,55 @@
+namespace DynamicSQL;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq.Expressions;
+using System.Reflection;
+
+internal static class ReaderHelper
+{
+    private static readonly IReadOnlyDictionary<Type, MethodInfo> GetValueMethodMap = new Dictionary<Type, MethodInfo>
+    {
+        { typeof(byte), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetByte)) },
+        { typeof(bool), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetBoolean)) },
+        { typeof(char), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetChar)) },
+        { typeof(short), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetInt16)) },
+        { typeof(int), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetInt32)) },
+        { typeof(long), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetInt64)) },
+        { typeof(float), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetFloat)) },
+        { typeof(double), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetDouble)) },
+        { typeof(decimal), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetDecimal)) },
+        { typeof(Guid), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetGuid)) },
+        { typeof(DateTime), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetDateTime)) },
+        { typeof(string), typeof(DbDataReader).GetMethod(nameof(DbDataReader.GetString)) },
+    };
+
+    private static readonly MethodInfo IsDbNullMethod = typeof(DbDataReader).GetMethod(nameof(DbDataReader.IsDBNull))!;
+
+    public static Expression CreateDataReaderGetValueExpression(Expression dataReaderExp, Type fieldType, int? columnOrdinal)
+    {
+        var innerType = Nullable.GetUnderlyingType(fieldType);
+
+        var callGetValueExp = Expression.Call(
+            dataReaderExp,
+            GetValueMethodMap[innerType ?? fieldType],
+            Expression.Constant(columnOrdinal));
+
+        var defaultValueExp = Expression.Default(fieldType);
+
+        if (columnOrdinal is null)
+        {
+            return defaultValueExp;
+        }
+
+        if (innerType is null && fieldType != typeof(string))
+        {
+            return callGetValueExp;
+        }
+
+        return Expression.Condition(
+            Expression.IsTrue(Expression.Call(dataReaderExp, IsDbNullMethod, Expression.Constant(columnOrdinal))),
+            defaultValueExp,
+            Expression.Convert(callGetValueExp, fieldType));
+    }
+}
