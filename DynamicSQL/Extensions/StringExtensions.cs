82c40e6e2e6687db9948de4fc1c4ff0210448d4b
Filipe Esch <filipe.esch@farfetch.com>
2024-01-31 12:58:41 +0000

    fix literal IN operator

--- DynamicSQL/Extensions/StringExtensions.cs
+++ DynamicSQL/Extensions/StringExtensions.cs
@@ -1,13 +1,19 @@
 namespace DynamicSQL.Extensions;
 
 using System.Collections.Generic;
-using DynamicSQL.Parser;
+using System.Linq;
+using System.Text.RegularExpressions;
 using DynamicSQL.Parser.Expressions;
 
 internal static class StringExtensions
 {
+    private static readonly Regex TokensRegex = new(@"<<|>>|\{|\}|\?|:|\s+IN\s+", RegexOptions.Singleline);
+
     public static IEnumerable<TokenExpression> Tokenize(this string value)
     {
-        return new TokenizerEnumerable(value);
+        return TokensRegex
+            .Matches(value)
+            .Cast<Match>()
+            .Select(match => TokenExpression.FromSymbol(match.Value.Trim(), match.Index, match.Index + match.Length - 1));
     }
 }
